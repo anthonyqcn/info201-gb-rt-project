049e80a62a087b55bb84a588dd9ec964e04a514e
Brandon Lee <leedong@uw.edu>
2018-12-04 00:49:01 -0800

    streams.R added

---
+++ streams.R
@@ -0,0 +1,17 @@
+source("data_analysis.R")
+
+nowhere <- length(grep("Nowhere!", movie_data$stream))
+netflix <- length(grep("Netflix", movie_data$stream))
+itunes <- length(grep("iTunes", movie_data$stream))
+amazon_instant <- length(grep("Amazon Instant", movie_data$stream))
+amazon_prime <- length(grep("Amazon Prime", movie_data$stream))
+
+services <- c(netflix, itunes, amazon_instant, amazon_prime, nowhere)
+names <- c("Netflix", "iTunes", "Amazon Instant", "Amazon Prime", "Nowhere")
+colors <- c("Red", "White", "Light Blue", "Blue", "Black")
+barplot(services,
+        names.arg = names,
+        xlab = "Streaming Services",
+        ylab = "# of Movies",
+        main = "# of Movies Streamed by Services in Top 100 Movies",
+        col = colors)
